f330b1f35008c9ca3f0e2bc863f53ea1f5fcac34
frankfolabi <75861776+frankfolabi@users.noreply.github.com>
2025-04-30 23:07:58 +0100

    Fix Setup script

--- inspire-terminal/setup.sh
+++ inspire-terminal/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Move all *_tips files to the user's home directory
-mv ./*_tips /home/$USER
+mkdir /home/$USER/.tips
+mv ./*_tips /home/$USER/.tips
 
 # Write the following to the .bashrc 
 tee -a ~/.bashrc <<EOF
-echo "shuf -n 1 /home/\$USER/inspiration_tips" 
-echo "" 
-echo "shuf -n 1 /home/\$USER/devops_tips"
+shuf -n 1 /home/\$USER/.tips/inspiration_tips 
+echo "---" 
+shuf -n 1 /home/\$USER/.tips/devops_tips
 EOF
 
 # Reload the .bashrc
