a8f65e3a8fea59bb7998732974ffb9b068ee3752
frankfolabi <75861776+frankfolabi@users.noreply.github.com>
2025-05-01 16:05:07 +0100

    Add logic to install and uninstall

--- inspire-terminal/install.sh
+++ inspire-terminal/install.sh
@@ -4,17 +4,24 @@
 mkdir -p "$HOME/.tips"
 cp ./*_tips "$HOME/.tips"
 
+# Check if Inspire Terminal already installed
+if grep -Fxq '# Inspire Terminal' "$HOME/.bashrc"; then
+  echo "Inspire Terminal is already installed. Exiting . . ."
+  exit 1
+else
 # Append block to .bashrc 
-tee -a "$HOME/.bashrc" <<'EOF'
+tee -a "$HOME/.bashrc" > /dev/null <<'EOF'
 
 # Inspire Terminal
 if [ -f "$HOME/.tips/inspiration_tips" ] && [ -f "$HOME/.tips/devops_tips" ]; then
-    shuf -n 1 $HOME/.tips/inspiration_tips 
-    echo "---" 
-    shuf -n 1 $HOME/.tips/devops_tips
+   shuf -n 1 $HOME/.tips/inspiration_tips 
+   echo "---" 
+   shuf -n 1 $HOME/.tips/devops_tips
 fi
 EOF
+fi
 
 # Reload .bashrc
 source "$HOME/.bashrc"
 
+echo "Installation completed. Enjoy your terminal."
--- inspire-terminal/uninstall.sh
+++ inspire-terminal/uninstall.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Remove Inspire Terminal from .bashrc
-sed -i '/# Inspire Terminal/,/fi/d' "$HOME/.bashrc"
-rm -rf "$HOME/.tips"
+# Uninstall Inspire Terminal
+if grep -Fxq '# Inspire Terminal' "$HOME/.bashrc"; then
+  # Remove Inspire Terminal from .bashrc
+  sed -i '/# Inspire Terminal/,/fi/d' "$HOME/.bashrc"
+  rm -rf "$HOME/.tips"
 
-# Reload .bashrc
-source "$HOME/.bashrc"
+  # Reload .bashrc
+  source "$HOME/.bashrc"
+  echo "Uninstallation complete. See you again."
 
-echo "Uninstallation Complete"
+else
+  echo "You don't have the Inspire Terminal."
+  echo "Do you want to install it? (y/n)"
+  read -n 1 -s answer
+  if [[ $answer == [Yy] ]]; then
+    bash install.sh
+  else
+    echo "Inspire Terminal not installed."
+    exit 1
+  fi
+fi
